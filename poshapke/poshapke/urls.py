5213f23df063b7f2ad22f73b9853cc45e95b1d5e
Серей Овинцев <ozonti@mail.ru>
2023-12-30 18:36:25 +0300

    Создание Маршрутов

--- poshapke/poshapke/urls.py
+++ poshapke/poshapke/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from products.views import index, products
 from django.conf import settings
 from django.conf.urls.static import static
 
