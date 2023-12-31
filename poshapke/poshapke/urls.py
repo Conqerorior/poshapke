344f3811a3df1a234f3886e558d4db1391343e69
Серей Овинцев <ozonti@mail.ru>
2023-12-31 19:29:29 +0300

    Создание Маршрутов

--- poshapke/poshapke/urls.py
+++ poshapke/poshapke/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls', namespace='products'))
+    path('', include('products.urls', namespace='products'))
 ]
 
 if settings.DEBUG:
