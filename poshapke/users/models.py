91b54ec5571ed5472df34cf73752a198a529718d
Серей Овинцев <ozonti@mail.ru>
2024-01-09 10:29:23 +0300

    Создание Модели Пользователь

--- poshapke/users/models.py
+++ poshapke/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Users(AbstractUser):
+    image = models.ImageField(
+        verbose_name='Изображение пользователя',
+        upload_to='users_image',
+        help_text='Загрузите Изображение'
+    )
