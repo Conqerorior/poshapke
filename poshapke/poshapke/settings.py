d61c7a1e5cf62964f36ec6d690be9ef17012ea37
Серей Овинцев <ozonti@mail.ru>
2024-01-09 10:21:45 +0300

    Создание Модели Пользователь

--- poshapke/poshapke/settings.py
+++ poshapke/poshapke/settings.py
@@ -24,7 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'products.apps.ProductsConfig'
+    'products.apps.ProductsConfig',
+    'users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
