46dc9c19c66fdd316fabb9b23354d2df3773ae60
Серей Овинцев <ozonti@mail.ru>
2024-01-10 14:41:42 +0300

    Авторизация

--- poshapke/users/views.py
+++ poshapke/users/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.forms import UserLoginForm
 
 
 def login(request):
-    context = {'form': UserLoginForm()}
-    return render(request, 'users/login.html', context=context)
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('products:index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
