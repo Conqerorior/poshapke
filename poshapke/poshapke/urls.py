f80e94514fbdac828787ed467cab9983fbc4298a
Серей Овинцев <ozonti@mail.ru>
2024-01-10 14:53:09 +0300

    Изменение Маршрутов

--- poshapke/poshapke/urls.py
+++ poshapke/poshapke/urls.py
@@ -4,9 +4,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='index'),
     path('', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users'))
 ]
