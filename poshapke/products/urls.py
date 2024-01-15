328217c59ba6d7767dc7b735baca461a076e52d2
Серей Овинцев <ozonti@mail.ru>
2024-01-15 13:58:08 +0300

    Создание приложения Корзина

--- poshapke/products/urls.py
+++ poshapke/products/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from basket.views import basket_add, basket_remove
 from products.views import products
@@ -7,6 +7,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('products/', products, name='products'),
-    path('products/add/<str:slug>/', basket_add, name='basket_add'),
-    path('products/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
+    path('products/', include('basket.urls', namespace='basket'))
+    # path('products/add/<str:slug>/', basket_add, name='basket_add'),
+    # path('products/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
