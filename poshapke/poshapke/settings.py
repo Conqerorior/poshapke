dcfa02a95b59d8494c68f9d7be615462886e65d6
Серей Овинцев <ozonti@mail.ru>
2024-01-16 19:10:17 +0300

    Создание контролера отправки по e-mail

--- poshapke/poshapke/settings.py
+++ poshapke/poshapke/settings.py
@@ -110,4 +110,9 @@ LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
 # Send e-mail
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
