67636e31b78816a845620cc7c0e3265487959f33
Серей Овинцев <ozonti@mail.ru>
2024-01-16 14:13:45 +0300

    Создание Админ Таблиц Верификации по е-mail

--- poshapke/users/admin.py
+++ poshapke/users/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Users
+from .models import Users, EmailVerification
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'url_uuid',
+        'created',
+    )
+    fields = (
+        'user',
+        'url_uuid',
+        'created',
+        'expiration'
+    )
+    readonly_fields = (
+        'expiration',
+    )
 
 
 admin.site.register(Users)
