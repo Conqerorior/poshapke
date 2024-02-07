ff1a38a3465ef435ab58bc70320ad716a2d59b38
Серей Овинцев <ozonti@mail.ru>
2024-02-07 18:03:37 +0300

    Добавление Celery

--- poshapke/users/forms.py
+++ poshapke/users/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import (AuthenticationForm, UserChangeForm,
-                                       UserCreationForm)
+from django.contrib.auth.forms import (
+    AuthenticationForm,
+    UserChangeForm,
+    UserCreationForm,
+)
 
 from users.tasks import send_verification_email
 
