efa3c9b33287ea25a74ea4c8a00ce9e0f4d9b92c
Серей Овинцев <ozonti@mail.ru>
2024-02-07 15:40:01 +0300

    Установка django-toolbar и redis

--- poshapke/poshapke/settings.py
+++ poshapke/poshapke/settings.py
@@ -87,7 +87,6 @@ DATABASES = {
         'PORT': os.getenv('PORT'),
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
