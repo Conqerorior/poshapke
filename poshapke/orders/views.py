02d9af4e7ca0676984ed2c8ab32e22863bdc9386
Серей Овинцев <ozonti@mail.ru>
2024-02-08 17:22:37 +0300

    Создание Модели 'Заказы'

--- poshapke/orders/views.py
+++ poshapke/orders/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
+
+from orders.forms import OrderForm
 
 
-class OrderCreateView(TemplateView):
+class OrderCreateView(CreateView):
+    form_class = OrderForm
     template_name = 'orders/order-create.html'
